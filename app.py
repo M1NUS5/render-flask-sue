3c2e11a72c0b4ca8cfe9c493c52368e20898941f
M1NUS5 <jtorresalvarez124@gmail.com>
2026-06-18 09:23:39 -0600

    prueba CD Railway

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Proyecto desplegado en Railway"
+    return "CD funcionando en Railway"
 
 if __name__ == "__main__":
     app.run()
