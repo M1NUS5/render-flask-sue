ffca69740ce27df85a2f784cc968fd28ea86ad85
M1NUS5 <jtorresalvarez124@gmail.com>
2026-06-18 09:15:17 -0600

    Agregar Procfile para Railway

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Despliegue continuo funcionando correctamente"
+    return "Proyecto desplegado en Railway"
 
 if __name__ == "__main__":
     app.run()
